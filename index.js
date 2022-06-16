37d4660ebf0d62950df3a00f501db2ef9d3142c7
phinic925 <phinicnyangate480@gmail.com>
2022-06-16 17:18:49 +0100

    commit tree ad8f7351492f6394e09947b96721984ca6c1a756

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+function reverseString(word){
+const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+  
+
 }
+
+
 
 /* 
   Add your pseudocode here
